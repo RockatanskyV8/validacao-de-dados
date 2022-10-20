bacd12678ac12fb927ac91268b2a7eff832f7f01
lucas <luccasanttoz@gmail.com>
2022-10-20 10:11:41 -0300

    altera funções de busca e atribuições

--- validacao.py
+++ validacao.py
@@ -17,6 +17,9 @@ from sklearn.model_selection import StratifiedKFold
 
 from modelos import Split 
 from modelos import Modelos 
+from modelos import Explorador
+
+from scipy.stats import randint
 
 SEED = 301
 np.random.seed(SEED)
@@ -27,20 +30,29 @@ df = pd.read_csv(URI).drop(columns=["Unnamed: 0"], axis=1)
 df['modelo'] = df.idade_do_modelo + np.random.randint(-2, 3, size=10000)
 df.modelo = df.modelo + abs(df.modelo.min()) + 1
 
-x = df[["preco", "idade_do_modelo", "km_por_ano"]]
+x = df[["preco", "idade_do_modelo", "km_por_ano", "modelo"]]
 y = df["vendido"]
 
+espaco_de_parametros = {
+    "max_depth" : [3, 5],
+    "min_samples_split" : [32, 64, 128],
+    "min_samples_leaf" : [32, 64, 128],
+    "criterion" : ["gini", "entropy"]
+}
 #  dados = Split(158020, x, y)
 modelos = Modelos(158020, x, y)
+explorador = Explorador(158020, espaco_de_parametros, x, y)
 
-
-cvKFold           = KFold(n_splits = 10)
+# cvKFold           = KFold(n_splits = 10)
 #  cvStratifiedKFold = StratifiedKFold(**kwargs)
-#  cvGroupKFold      = GroupKFold(**kwargs)
+cvGroupKFold      = GroupKFold(n_splits = 10)
 
-arvore_decisao_2d = modelos.arvore_decisao_2d(cvKFold, df.modelo, 2, 128)
-arvore_decisao_3d = modelos.arvore_decisao_3d(cvKFold, df.modelo, 2, 128, 128)
+busca = explorador.busca(cvGroupKFold, x.modelo)
+resultado = pd.DataFrame(busca.cv_results_)
+#  arvore_decisao_2d = modelos.arvore_decisao_2d(cvKFold, df.modelo, 2, 128)
+#  arvore_decisao_3d = modelos.arvore_decisao_3d(cvKFold, df.modelo, 2, 128, 128)
 
-print(arvore_decisao_2d)
-print(arvore_decisao_3d)
+#  print(arvore_decisao_2d)
+#  print(arvore_decisao_3d)
+
 
